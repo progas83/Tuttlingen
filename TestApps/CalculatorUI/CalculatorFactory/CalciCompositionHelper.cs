cb23966c6bd56de565c3584d82da0f4b2b96c278
progas83 <progas83@gmail.com>
2016-07-10 18:29:23 +0200

    from inside classes

---
+++ TestApps/CalculatorUI/CalculatorFactory/CalciCompositionHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Reflection;
+using CalculatorContract;
+
+namespace CompositionHelper
+{
+    public class CalciCompositionHelper
+    {
+        [Import(typeof(ICalculator))]
+        public ICalculator CalciPlugin { get; set; }
+
+        /// <summary>
+        /// Assembles the calculator components
+        /// </summary>
+        public void AssembleCalculatorComponents()
+        {
+            try
+            {
+                //Step 1: Initializes a new instance of the 
+                //        System.ComponentModel.Composition.Hosting.AssemblyCatalog class with the 
+                //        current executing assembly.
+                var catalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
+
+                //Step 2: The assemblies obtained in step 1 are added to the CompositionContainer
+                var container = new CompositionContainer(catalog);
+
+                //Step 3: Composable parts are created here i.e. the Import and Export components 
+                //        assembles here
+                container.ComposeParts(this);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Sends the result back to the client
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public int GetResult(int num1, int num2)
+        {
+           return CalciPlugin.GetNumber(num1, num2);
+        }
+    }
+}
---
+++ TestApps/CalculatorUI/CalculatorUI/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using CompositionHelper;
+
+
+namespace CalculatorUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CalciCompositionHelper objCompHelper;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+            objCompHelper = new CalciCompositionHelper();
+
+            //Assembles the calculator components that will participate in composition
+            objCompHelper.AssembleCalculatorComponents();
+
+            //Gets the result
+            var result = objCompHelper.GetResult(Convert.ToInt32(txtFirstNumber.Text), Convert.ToInt32(txtSecondNumber.Text));
+
+            //Display the result
+            txtResult.Text = result.ToString();
+        }
+    }
+}
