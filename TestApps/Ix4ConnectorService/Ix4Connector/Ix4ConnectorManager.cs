a118b5c46dc15e4e731b59759a5234d4b051d32a
progas83 <progas83@gmail.com>
2016-07-12 17:13:52 +0200

    add webService reference

---
+++ TestApps/Ix4ConnectorService/Ix4Connector/Ix4ConnectorManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ix4Connector
+{
+    public class Ix4ConnectorManager
+    {
+        private static object _padlock = new object();
+        private static Ix4ConnectorManager _manager;
+        public static Ix4ConnectorManager Instance
+
+        {
+            get
+            {
+                if (_manager == null)
+                {
+                    lock (_padlock)
+                    {
+                        if (_manager == null)
+                        {
+                            _manager = new Ix4ConnectorManager();
+                        }
+                    }
+                }
+                return _manager;
+            }
+        }
+    }
+}
