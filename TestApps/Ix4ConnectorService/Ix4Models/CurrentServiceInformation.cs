dae358cb655bba0bed9216d3e42131cb530d5fde
progas83 <progas83@gmail.com>
2016-08-04 04:12:36 +0200

    ui changes

--- TestApps/Ix4ConnectorService/Ix4Models/CurrentServiceInformation.cs
+++ TestApps/Ix4ConnectorService/Ix4Models/CurrentServiceInformation.cs
@@ -13,7 +13,7 @@ namespace Ix4Models
         // remote db connection server=sip1.skypro.eu;database=GWTalkDB;userid=UserReadDB;password=Asdf1234;Allow User Variables=True
 
         private static readonly string _serviceDescription = "Navitel to ix4 adapter service";
-        public const string ServiceName = "Navision to ix4 connector";// { get { return _serviceName; } }
+        public const string ServiceName = "Ix4-Agent";// { get { return _serviceName; } }
 
         public static string ServiceDescription { get { return _serviceDescription; } }
 
